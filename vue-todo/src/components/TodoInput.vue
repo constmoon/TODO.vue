a1084952b08028a31fcb4bcdbdcd9f617c5f137f
constmoon <jiiw.kang@gmail.com>
2019-01-08 03:29:00 +0900

    save and load inputs

--- vue-todo/src/components/TodoInput.vue
+++ vue-todo/src/components/TodoInput.vue
@@ -1,8 +1,11 @@
 <template>
-    <div>
+    <div class="inputBox shadow">
         <!-- v-model="특정 데이터 속성": input에 입력된 텍스트값을 동적으로 vue instance에 맵핑 -->
-        <input type="text" v-model="newTodoItem">
-        <button v-on:click="addTodo">add</button>
+        <input type="text" v-model="newTodoItem" v-on:keyup.enter="addTodo">
+        <!-- <button v-on:click="addTodo">add</button> -->
+        <span class="addContainer" v-on:click="addTodo">
+            <i class="fas fa-plus addBtn"></i>
+        </span>
     </div>
 </template>
 
@@ -24,6 +27,30 @@ export default {
 }
 </script>
 
-<style>
+<style scoped>
+input:focus{
+    outline: none;
+}
+.inputBox{
+    background: #fff;
+    height: 50px;
+    line-height: 50px;
+    border-radius: 5px;
+}
+.inputBox input{
+    border-style: none;
+    font-size: 0.9rem;
+}
+.addContainer{
+    float: right;
+    background: linear-gradient(to right, #6478FB, #8763FB);
+    display: block;
+    width: 3rem;
+    border-radius: 0 5px 5px 0;
+}
+.addBtn{
+    color: #fff;
+    vertical-align: middle;
+}
 
 </style>
---
+++ vue-todo/src/components/TodoList.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+      <ul>
+          <!-- v-bind:key => todoItem의 텍스트가 key가 되어 중복 X, v-for의 성능 가속화 -->
+          <li v-for="todoItem in todoItems" v-bind:key="todoItem">
+              {{ todoItem }}
+          </li>
+          
+      </ul>
+  </div>
+</template>
+
+<script>
+export default {
+    // 로컬 스토리지에서 꺼내서 담을 데이터
+    data: function(){
+        return{
+            todoItems: []
+        }
+    },
+    // 인스턴스가 생성되자마자 호출됨
+    created: function(){
+        if(localStorage.length > 0){
+            for(var i=0; i<localStorage.length; i++){
+                // webpack dev server를 제외한 나머지들 저장
+                if(localStorage.key(i) !== 'loglevel:webpack-dev-server'){
+                    // this로 같은 인스턴스의 todoItems에 접근
+                    this.todoItems.push(localStorage.key(i));
+                }
+            }
+        }
+    }
+};
+</script>
+
+<style>
+</style>
