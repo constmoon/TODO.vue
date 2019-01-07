760a389d8b27f8dceff9f807378aee2278b5e466
constmoon <jiiw.kang@gmail.com>
2019-01-08 03:05:01 +0900

    git 충돌 수정

---
+++ vue-todo/src/components/TodoHeader.vue
@@ -0,0 +1,23 @@
+<template>
+    <!-- html -->
+    <header>
+        <h1>TODO it!</h1>
+    </header>
+</template>
+
+<script>
+/* JS script */
+export default {
+
+}
+</script>
+
+<style scoped>
+/* 해당 컴포넌트 안에서만 유효한 CSS */
+h1{
+    color: #000;
+    font-weight: 900;
+    margin: 2.5rem 0 1.5rem;
+}
+
+</style>
---
+++ vue-todo/src/components/TodoInput.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+        <!-- v-model="특정 데이터 속성": input에 입력된 텍스트값을 동적으로 vue instance에 맵핑 -->
+        <input type="text" v-model="newTodoItem">
+        <button v-on:click="addTodo">add</button>
+    </div>
+</template>
+
+<script>
+export default {
+    data: function(){
+        return{
+            newTodoItem: ""
+        }
+    },
+    methods:{
+        addTodo: function(){
+            localStorage.setItem(this.newTodoItem, this.newTodoItem);    // 로컬 스토리지에 저장
+        },
+        clearInput: function(){
+            this.newTodoItem = '';     // 저장 후 input box 초기화
+        },
+    }
+}
+</script>
+
+<style>
+
+</style>
